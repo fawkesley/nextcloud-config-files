bdd0a354814cc73d127866f10c8bdf3b77d07106
Paul M Furley <paul@paulfurley.com>
2016-12-14 22:36:11 +0000

    Add (redacted) nextcloud config.php

---
+++ var/www/nextcloud/config/config.php
@@ -0,0 +1,24 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc3iyxgu2ctc',
+  'passwordsalt' => '<removed>',
+  'secret' => '<removed>',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.0.191',
+    1 => 'cloud.paulfurley.com',
+  ),
+  'datadirectory' => '/media/externalhd/nextcloud',
+  'overwrite.cli.url' => 'https://cloud.paulfurley.com/nextcloud',
+  'dbtype' => 'pgsql',
+  'version' => '9.1.1.5',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'nextcloud',
+  'dbpassword' => '<removed>',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+  'loglevel' => "2",
+);
